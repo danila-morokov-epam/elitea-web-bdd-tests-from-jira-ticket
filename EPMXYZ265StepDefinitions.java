748b23ca8b8c888546ecd63584b431dccf7a4e24
danila-morokov-epam <danila_morokov@epam.com>
2025-02-17 14:50:47 -0600

    Create EPMXYZ265StepDefinitions.java

---
+++ EPMXYZ265StepDefinitions.java
@@ -0,0 +1,51 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class EPMXYZ265StepDefinitions {
+
+    @Given("I am on the homepage")
+    public void iAmOnTheHomepage() {
+        // Code to navigate to the homepage
+    }
+
+    @When("I click on the \"Solutions\" dropdown menu in the main navigation bar")
+    public void iClickOnTheSolutionsDropdownMenu() {
+        // Code to click on the Solutions dropdown menu
+    }
+
+    @Then("I should see the options \"Automotive,\" \"Lifestyle,\" \"Consumer,\" \"Professional,\" and \"Digital Transformation Solutions\"")
+    public void iShouldSeeTheOptions() {
+        // Code to verify the dropdown options
+    }
+
+    @When("I click on the \"Automotive\" option")
+    public void iClickOnTheAutomotiveOption() {
+        // Code to click on the Automotive option
+    }
+
+    @Then("I should be navigated to the \"Automotive\" section")
+    public void iShouldBeNavigatedToTheAutomotiveSection() {
+        // Code to verify navigation to the Automotive section
+    }
+
+    @Given("I have opened the \"Solutions\" dropdown menu")
+    public void iHaveOpenedTheSolutionsDropdownMenu() {
+        // Code to ensure the Solutions dropdown menu is open
+    }
+
+    @When("I click outside the dropdown menu")
+    public void iClickOutsideTheDropdownMenu() {
+        // Code to simulate clicking outside the dropdown menu
+    }
+
+    @Then("the \"Solutions\" dropdown menu should close")
+    public void theSolutionsDropdownMenuShouldClose() {
+        // Code to verify the dropdown menu is closed
+    }
+
+    @When("I click on another navigation item")
+    public void iClickOnAnotherNavigationItem() {
+        // Code to simulate clicking on another navigation item
+    }
+}
